1b52d8756d88bcaf5f83a1bb2b44afd92d0e4468
root <root@oro-vxfexp-01.luxoft.com>
2019-06-26 18:49:00 +0300

    pd_ label fixes and more metrics

--- vxflexos-exporter.rb
+++ vxflexos-exporter.rb
@@ -39,37 +39,41 @@ class VxFlexOSExporter
       request = session.gets
 
       if request && (parts = request.split(' ')) && parts.size > 1
-        puts request
-        if parts[1] == '/'
-          session.print "HTTP/1.1 200\r\n"
-          session.print "Content-Type: text/html\r\n"
-          session.print "\r\n"
-          session.print '<html>
-              <head><title>VXFlexOS Exporter</title></head>
-              <body>
-                <h1>VXFlexOS Exporter</h1>
-                <p><a href="/metrics">Metrics</a></p>
-              </body>
-            </html>'
-        elsif parts[1] == '/metrics'
-          session.print "HTTP/1.1 200\r\n"
-          session.print "Content-Type: text/plaintext\r\n"
-          session.print "\r\n"
+        begin
+          puts request
+          if parts[1] == '/'
+            session.print "HTTP/1.1 200\r\n"
+            session.print "Content-Type: text/html\r\n"
+            session.print "\r\n"
+            session.print '<html>
+                <head><title>VXFlexOS Exporter</title></head>
+                <body>
+                  <h1>VXFlexOS Exporter</h1>
+                  <p><a href="/metrics">Metrics</a></p>
+                </body>
+              </html>'
+          elsif parts[1] == '/metrics'
+            session.print "HTTP/1.1 200\r\n"
+            session.print "Content-Type: text/plaintext\r\n"
+            session.print "\r\n"
 
-          @stats_processed = []
-          get_auth_token
-          get_tree
-          process_tree
-          get_stats
-          process_stats
-          convert_kb_iops
+            @stats_processed = []
+            get_auth_token
+            get_tree
+            process_tree
+            get_stats
+            process_stats
+            convert_kb_iops
 
-          output_stats(session)
-        else
-          session.print "HTTP/1.1 404\r\n"
-          session.print "Content-Type: text/plaintext\r\n"
-          session.print "\r\n"
-          session.print "Not Found! VXFlexOS Exporter only listens on /metrics"
+            output_stats(session)
+          else
+            session.print "HTTP/1.1 404\r\n"
+            session.print "Content-Type: text/plaintext\r\n"
+            session.print "\r\n"
+            session.print "Not Found! VXFlexOS Exporter only listens on /metrics"
+          end
+        rescue Exception => e
+          puts "Exception: #{e}"
         end
       end
 
@@ -182,32 +186,32 @@ class VxFlexOSExporter
                    sdc_name: sdc['name']})
     elsif type == 'ProtectionDomain'
       protection_domain = @tree['protectionDomainList'].select{|pdo| pdo['id'] == device_id}.first
-      tags.merge!({pdo_id: device_id,
-                   pdo_name: protection_domain['name']})
+      tags.merge!({pd_id: device_id,
+                   pd_name: protection_domain['name']})
     elsif type == 'Sds'
-      protection_domain_id = @tree['sdsList'].first{|sds| sds.id == device_id}['protectionDomainId']
+      protection_domain_id = @tree['sdsList'].select{|sds| sds['id'] == device_id}.first['protectionDomainId']
       protection_domain = @tree['protectionDomainList'].select{|pdo| pdo['id'] == protection_domain_id}.first
       sds = @tree['sdsList'].select{|sds| sds['id'] == device_id}.first
-      tags.merge!({pdo_id: protection_domain_id,
-                   pdo_name: protection_domain['name'],
+      tags.merge!({pd_id: protection_domain_id,
+                   pd_name: protection_domain['name'],
                    sds_id: device_id,
                    sds_name: sds['name']})
     elsif type == 'StoragePool'
-      protection_domain_id = @tree['storagePoolList'].first{|sto| sto.id == device_id}['protectionDomainId']
+      protection_domain_id = @tree['storagePoolList'].select{|sto| sto['id'] == device_id}.first['protectionDomainId']
       protection_domain = @tree['protectionDomainList'].select{|pdo| pdo['id'] == protection_domain_id}.first
       storage_pool = @tree['storagePoolList'].select{|sto| sto['id'] == device_id}.first
-      tags.merge!({pdo_id: protection_domain_id,
-                   pdo_name: protection_domain['name'],
+      tags.merge!({pd_id: protection_domain_id,
+                   pd_name: protection_domain['name'],
                    stp_id: device_id,
                    stp_name: storage_pool['name']})
     elsif type == 'Volume'
-      storage_pool_id = @tree['volumeList'].first{|vol| vol.id == device_id}['storagePoolId']
+      storage_pool_id = @tree['volumeList'].select{|vol| vol['id'] == device_id}.first['storagePoolId']
       storage_pool = @tree['storagePoolList'].select{|sto| sto['id'] == storage_pool_id}.first
       protection_domain_id = storage_pool['protectionDomainId']
       protection_domain = @tree['protectionDomainList'].select{|pdo| pdo['id'] == protection_domain_id}.first
       volume = @tree['volumeList'].select{|vol| vol['id'] == device_id}.first
-      tags.merge!({pdo_id: protection_domain_id,
-                   pdo_name: protection_domain['name'],
+      tags.merge!({pd_id: protection_domain_id,
+                   pd_name: protection_domain['name'],
                    stp_id: storage_pool_id,
                    stp_name: storage_pool['name'],
                    vol_id: device_id,
@@ -220,8 +224,8 @@ class VxFlexOSExporter
       sds = @tree['sdsList'].select{|sds| sds['id'] == sds_id}.first
       protection_domain_id = storage_pool['protectionDomainId']
       protection_domain = @tree['protectionDomainList'].select{|pdo| pdo['id'] == protection_domain_id}.first
-      tags.merge!({pdo_id: protection_domain_id,
-                   pdo_name: protection_domain['name'],
+      tags.merge!({pd_id: protection_domain_id,
+                   pd_name: protection_domain['name'],
                    stp_id: storage_pool_id,
                    stp_name: storage_pool['name'],
                    sds_id: sds_id,
@@ -235,8 +239,8 @@ class VxFlexOSExporter
       sds = @tree['sdsList'].select{|sds| sds['id'] == sds_id}.first
       protection_domain_id = sds['protectionDomainId']
       protection_domain = @tree['protectionDomainList'].select{|pdo| pdo['id'] == protection_domain_id}.first
-      tags.merge!({pdo_id: protection_domain_id,
-                   pdo_name: protection_domain['name'],
+      tags.merge!({pd_id: protection_domain_id,
+                   pd_name: protection_domain['name'],
                    sds_id: sds_id,
                    sds_name: sds['name'],
                    rfdev_id: device_id,
